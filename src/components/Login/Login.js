4d73a13e881a3ff7d88277c0783880406597084b
californikate <regina.plalange8@yandex.ru>
2023-09-02 20:51:55 +0500

    исправляет логин, добавляет регистрацию

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,30 +6,30 @@ import logo from '../../images/logo.svg';
 
 function Login() {
   return(
-    <section className="login">
-      <form className="login__form" action="#">
-        <Link className="login__logo-link" to={"/"}>
-          <img className="login__logo" src={ logo } alt="логотип сайта." />
+    <section className="auth">
+      <form className="auth__form" action="#">
+        <Link className="auth__logo-link" to={"/"}>
+          <img className="auth__logo" src={ logo } alt="логотип сайта." />
         </Link>
-        <h2 className="login__title">Рады видеть!</h2>
-        <label for="email" className="login__form-label">E-mail</label>
+        <h2 className="auth__title">Рады видеть!</h2>
+        <label for="email" className="auth__form-label">E-mail</label>
         <input 
           id="email"
           name="email"
           type="email"
-          className="login__form-input"
+          className="auth__form-input"
           required
         />
-        <label for="password" className="login__form-label">Пароль</label>
+        <label for="password" className="lauth__form-label">Пароль</label>
         <input
           id="password" 
           name="password" 
           type="password"
-          className="login__form-input"
+          className="auth__form-input"
           required
         />
-        <button type="submit" className="login__button button">Войти</button>
-        <Link to="/sign-up" className="login__link">Еще не зарегистрированы? Регистрация</Link>
+        <button type="submit" className="auth__button button">Войти</button>
+        <Link to="/sign-up" className="auth__link">Еще не зарегистрированы? Регистрация</Link>
       </form>
     </section>
   );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,1 +1,48 @@
 // компонент страницы регистрации
+
+import './Register.css';
+import { Link } from "react-router-dom";
+import logo from '../../images/logo.svg';
+
+function Register() {
+  return(
+    <section className="auth">
+      <form className="auth__form" action="#">
+        <Link className="auth__logo-link" to={"/"}>
+          <img className="auth__logo" src={ logo } alt="логотип сайта." />
+        </Link>
+        <h2 className="auth__title">Добро пожаловать!</h2>
+        <label for="name" className="auth__form-label">Имя</label>
+        <input 
+          id="name"
+          name="name"
+          type="name"
+          minLength="2"
+          maxLength="30"
+          className="auth__form-input"
+          required
+        />
+        <label for="email" className="auth__form-label">E-mail</label>
+        <input 
+          id="email"
+          name="email"
+          type="email"
+          className="auth__form-input"
+          required
+        />
+        <label for="password" className="lauth__form-label">Пароль</label>
+        <input
+          id="password" 
+          name="password" 
+          type="password"
+          className="auth__form-input"
+          required
+        />
+        <button type="submit" className="auth__button button">Войти</button>
+        <Link to="/sign-up" className="auth__link">Еще не зарегистрированы? Регистрация</Link>
+      </form>
+    </section>
+  );
+}
+
+export default Register;
