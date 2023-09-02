cfe39a8c541834e1a5c2a5e7d18824bd71d445c7
californikate <regina.plalange8@yandex.ru>
2023-09-03 01:54:09 +0500

    добавляет футер

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,34 @@
 
 import React from "react";
 import './Footer.css';
+import { Link } from "react-router-dom";
 
 function Footer() {
   return(
-    <header className="footer">
-      <h2>test2</h2>
-    </header>
+    <footer className="footer">
+      <div className="footer__container">
+        <h3 className="footer__header">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+        <div className="footer__info">
+          <p className="footer__copyright">© {new Date().getFullYear()}</p>
+          <nav className="footer__nav">
+            <Link
+              to={"https://practicum.yandex.ru/"}
+              target="_blank"
+              className="footer__link"
+            >
+              Яндекс.Практикум
+            </Link>
+            <Link
+              to={"https://github.com/californikate"}
+              target="_blank"
+              className="footer__link"
+            >
+              Github
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </footer>
   )
 }
 
