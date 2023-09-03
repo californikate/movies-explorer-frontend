f38753635409ed639aa23acfc6f595e54b1c6ca0
californikate <regina.plalange8@yandex.ru>
2023-09-03 14:43:34 +0500

    добавляет секцию about project, доделать timeline

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -1,9 +1,32 @@
 // компонент с описанием дипломного проекта
+import './AboutProject.css';
 
 function AboutProject() {
   return(
     <section className="about-project">
-      <div className="about-project__container"></div>
+      <div className="about-project__container">
+        <h2 className="about-project__header">О проекте</h2>
+        <ul className="about-project__table">
+          <li className="about-project__table-item">
+            <h3 className="about-project__table-title">Дипломный проект включал 5 этапов</h3>
+            <p className="about-project__table-subtitle">
+              Составление плана, работу над бэкендом, вёрстку, добавление 
+              функциональности и финальные доработки.
+            </p>
+          </li>
+          <li>
+            <h3 className="about-project__table-title">На выполнение диплома ушло 5 недель</h3>
+            <p className="about-project__table-subtitle">
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно 
+              было соблюдать, чтобы успешно защититься.
+            </p>
+          </li>
+        </ul>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 }
