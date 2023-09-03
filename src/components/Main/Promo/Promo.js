c27d47a7017e3965de0f90deb875071e4e889051
californikate <regina.plalange8@yandex.ru>
2023-09-03 16:45:42 +0500

    добавляет promo.css

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -5,7 +5,7 @@ function Promo() {
   return(
     <section className="promo">
       <div className="promo__container">
-
+        <h1 className="promo__header">Учебный проект студента факультета Веб-разработки.</h1>
       </div>
     </section>
   );
