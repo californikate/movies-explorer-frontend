815276e90fcd03655284097578fb3aca2e335a37
californikate <regina.plalange8@yandex.ru>
2023-09-03 21:26:48 +0500

    pagenotfound компонент, стили, роут

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import './PageNotFound.css';
+
+function PageNotFound() {
+  return(
+    <section className="notfound">
+      <div className="notfound__container">
+        <h1 className="notfound__header">404</h1>
+        <p className="notfound__subtitle">Страница не найдена</p>
+        <Link to={"/"} className="notfound__link">Назад</Link>
+      </div>
+    </section>
+  );
+}
+
+export default PageNotFound;
