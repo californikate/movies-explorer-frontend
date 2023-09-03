81d7520bc812a0b623307e6f7205aeb6728f417b
californikate <regina.plalange8@yandex.ru>
2023-09-03 18:48:34 +0500

    techs.css & aboutme.css

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -6,20 +6,22 @@ import { Link } from 'react-router-dom';
 function AboutMe() {
   return(
     <section className="about-me" id="about-me">
+      <h2 className="about-me__header">Студент</h2>
       <div className="about-me__container">
-        <h2 className="about-me__header">Студент</h2>
-        <h3 className="about-me__title">Катерина</h3>
-        <p className="about-me__subtitle">Фронтенд-разработчик, 34 года</p>
-        <p className="about-me__description">
-          Я родился и живу в Саратове, закончил факультет экономики СГУ. 
-          У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. 
-          Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». 
-          После того, как прошёл курс по веб-разработке, начал заниматься 
-          фриланс-заказами и ушёл с постоянной работы.
-        </p>
-        <Link to={ "https://github.com/californikate" } target="_blank" className="about-me__link">
-          Github
-        </Link>
+        <div>
+          <h3 className="about-me__title">Катерина</h3>
+          <p className="about-me__subtitle">Фронтенд-разработчик, 34 года</p>
+          <p className="about-me__description">
+            Я родился и живу в Саратове, закончил факультет экономики СГУ. 
+            У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. 
+            Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». 
+            После того, как прошёл курс по веб-разработке, начал заниматься 
+            фриланс-заказами и ушёл с постоянной работы.
+          </p>
+          <Link to={ "https://github.com/californikate" } target="_blank" className="about-me__link">
+            Github
+          </Link>
+        </div>
         <img src={ photo } className="about-me__photo" alt="фото студента." />
       </div>
     </section>
--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -1,10 +1,12 @@
 //  компонент с использованными технологиями
+import './Techs.css';
 
 function Techs() {
   return(
     <section className="techs" id="techs">
+      <h2 className="techs__header">Технологии</h2>
       <div className="techs__container">
-        <h2 className="techs__header">Технологии</h2>
+
         <h3 className="techs__title">7 технологий</h3>
         <p className="techs__subtitle">
           На курсе веб-разработки мы освоили технологии, 
