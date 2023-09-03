a09b4995b564e961807983f3191e426294521341
californikate <regina.plalange8@yandex.ru>
2023-09-03 15:18:08 +0500

    добавляет секцию techs

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -3,7 +3,23 @@
 function Techs() {
   return(
     <section className="techs">
-      <div className="techs__container"></div>
+      <div className="techs__container">
+        <h2 className="techs__header">Технологии</h2>
+        <h3 className="techs__title">7 технологий</h3>
+        <p className="techs__subtitle">
+          На курсе веб-разработки мы освоили технологии, 
+          которые применили в дипломном проекте.
+        </p>
+        <ul className="techs__table">
+          <li className="techs__table-item">HTML</li>
+          <li className="techs__table-item">CSS</li>
+          <li className="techs__table-item">JS</li>
+          <li className="techs__table-item">React</li>
+          <li className="techs__table-item">Git</li>
+          <li className="techs__table-item">Express.js</li>
+          <li className="techs__table-item">mongoDB</li>
+        </ul>
+      </div>
     </section>
   );
 }
