816373e374d6dc303fa854cea4698e7fedde762f
californikate <regina.plalange8@yandex.ru>
2023-09-04 19:23:04 +0500

    techs.css адаптивная верстка

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -6,7 +6,6 @@ function Techs() {
     <section className="techs" id="techs">
       <h2 className="techs__header">Технологии</h2>
       <div className="techs__container">
-
         <h3 className="techs__title">7 технологий</h3>
         <p className="techs__subtitle">
           На курсе веб-разработки мы освоили технологии, 
