792bca12d7584794437a799777396acb8c92db80
californikate <regina.plalange8@yandex.ru>
2023-09-05 00:30:40 +0500

    исправляет верстку profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,40 +1,54 @@
 // компонент страницы изменения профиля
 import './Profile.css';
 
+import Header from '../Header/Header';
+
 function Profile() {
   return(
-    <section className="profile">
-      <h2 className="profile__header">Привет, Виталий!</h2>
-      <form className="profile__form">
-        <div className="profile__form-wrap">
-          <label for="name" className="profile__label">Имя</label>
-          <input
-            id="name" 
-            name="name" 
-            type="name"
-            minLength="2"
-            maxLength="30"
-            defaultValue={"Виталий" || ""}
-            className="profile__form-input"
-            required
-          />
-        </div>
-        <div className="profile__form-wrap">
-          <label for="email" className="profile__label">E-mail</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            defaultValue={"pochta@yandex.ru" || ""}
-            className="profile__form-input"
-            required
-          />
-        </div>
-      </form>
+    <>
+      <Header />
 
-      <button type="button" className="profile__edit-button button">Редактировать</button>
-      <button type="button" className="profile__exit-button">Выйти из аккаунта</button>
-    </section>
+      <section className="profile">
+        <h1 className="profile__header">Привет, Виталий!</h1>
+        <form className="profile__form">
+          <div className="profile__form-wrap">
+            <label for="name" className="profile__label">Имя</label>
+            <input
+              id="name" 
+              name="name" 
+              type="text"
+              minLength="2"
+              maxLength="30"
+              defaultValue={"Виталий" || ""}
+              className="profile__form-input"
+              required
+              disabled
+            />
+          </div>
+          <div className="profile__form-wrap">
+            <label for="email" className="profile__label">E-mail</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              defaultValue={"pochta@yandex.ru" || ""}
+              className="profile__form-input"
+              required
+              disabled
+            />
+          </div>
+        </form>
+
+        <ul className="profile__buttons">
+          <li>
+            <button type="button" className="profile__edit-button button">Редактировать</button>
+          </li>
+          <li>
+            <button type="button" className="profile__exit-button">Выйти из аккаунта</button>
+          </li>
+        </ul>
+      </section>
+    </>
   );
 }
 
