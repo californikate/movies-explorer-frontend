8bb1c15872fcc8aa830bd0dde7e8e9b7c0a966f4
californikate <regina.plalange8@yandex.ru>
2023-09-08 00:48:22 +0500

    preloader, movies card, разобраться с movies card list, адаптивной версткой, ui

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -3,9 +3,33 @@
 import React from "react";
 import './MoviesCardList.css';
 
+import { useLocation } from "react-router-dom";
+import  Movies from "../../utils/const";
+import MoviesCard from "../MoviesCard/MoviesCard";
+import Preloader from "../Preloader/Preloader";
+
 function MoviesCardList() {
+  const { pathname } = useLocation();
+
   return (
-    <section className="movies-cardlist"></section>
+    <section className="movies-cardlist">
+      <div className="movies-cardlist__container">
+        <ul className="movies-cardlist__table">
+          {Movies.map((card) => {
+            return (
+              <li key={ card._id }>
+                <MoviesCard movie={ card } />
+              </li>
+            )
+          })}
+        </ul>
+        
+        <button type="button" className="movies-cardlist__button button">
+            Ещё
+        </button>
+        <Preloader />
+      </div>
+    </section>
   );
 }
 
