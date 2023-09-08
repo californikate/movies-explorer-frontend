2632fe0d80b6307f6d136163042a22b943fab343
californikate <regina.plalange8@yandex.ru>
2023-09-08 15:24:36 +0500

    исправляет компонент и стили авторизации

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -7,11 +7,11 @@ import Logo from '../Logo/Logo';
 function Auth({ type }) {
   return (
     <section className="auth">
+      <Logo />
+      <h2 className="auth__title">
+        { type === "signin" ? "Рады видеть!" : "Добро пожаловать!" }
+      </h2>
       <form className="auth__form" action="#">
-        <Logo />
-        <h2 className="auth__title">
-          { type === "signin" ? "Рады видеть!" : "Добро пожаловать!" }
-        </h2>
         { type !== "signin" && (
           <>
             <label for="name" className="auth__form-label">Имя</label>
@@ -21,6 +21,7 @@ function Auth({ type }) {
               type="name"
               minLength="2"
               maxLength="30"
+              placeholder="Виталий"
               className="auth__form-input"
               required
             />
@@ -31,6 +32,7 @@ function Auth({ type }) {
           id="email"
           name="email"
           type="email"
+          placeholder="pochta@yandex.ru"
           className="auth__form-input"
           required
         />
@@ -39,23 +41,26 @@ function Auth({ type }) {
           id="password" 
           name="password" 
           type="password"
+          placeholder="••••••••••••••"
           className="auth__form-input"
           required
         />
-        <span className="auth__form-error">Что-то пошло не так...</span>
-        <>
-          <button type="submit" className="auth__button button">
-            { type === "signin" ? "Войти" : "Зарегистрироваться" }
-          </button>
-          <>
-            <span>
-              { type === "signin" ? "Еще не зарегистрированы?" : "Уже зарегистрированы?" }
-            </span>
-            <Link to={ type === "signin" ? "/signup" : "/signin" } className="auth__link">
-              { type === "signin" ? "Регистрация" : "Войти" }
-            </Link>
-          </>
-        </>
+
+        { type !== "signin" && (
+          <span className="auth__form-error">Что-то пошло не так...</span>
+        )}
+        
+        <button type="submit" className={ type === "signin" ? "auth__button auth__button_type_signin" : "auth__button" }>
+          { type === "signin" ? "Войти" : "Зарегистрироваться" }
+        </button>
+        <div className="auth__link-wrap">
+          <span className="auth__link-span">
+            { type === "signin" ? "Еще не зарегистрированы?" : "Уже зарегистрированы?" }
+          </span>
+          <Link to={ type === "signin" ? "/signup" : "/signin" } className="auth__link">
+            { type === "signin" ? "Регистрация" : "Войти" }
+          </Link>
+        </div>
       </form>
     </section>
   );
